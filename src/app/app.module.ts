b6521798a9c024b6426c593b5e158ead3b1a5827
VictoriaMarcelle <victoria.marcellemm@gmail.com>
2022-03-07 21:24:35 -0300

    Atv 02 Angular finalizada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputTextComponent } from './input-text/input-text.component';
+import { InputDateComponent } from './input-date/input-date.component';
+import { InputPasswordComponent } from './input-password/input-password.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputTextComponent,
+    InputDateComponent,
+    InputPasswordComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
